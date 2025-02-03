bcea84131574cc56f59818ed60a464b1b45194e0
fileini <francescomaria.falini@gmail.com>
2025-02-03 09:40:43 +0000

    refactor menu router

--- frontend/storysizer/lib/routes.dart
+++ frontend/storysizer/lib/routes.dart
@@ -1,6 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:storysizer/screens/groups.dart';
 import 'package:storysizer/screens/login.dart';
 import 'package:storysizer/screens/menu.dart';
+import 'package:storysizer/screens/mysizings.dart';
+import 'package:storysizer/screens/profile.dart';
+import 'package:storysizer/screens/quick_sizer.dart';
 import 'package:storysizer/services/auth_service.dart';
 
 class StszRoutes {
@@ -32,7 +37,54 @@ class StszRoutes {
         name: 'home',
         path: '/home',
         builder: (context, state) => const MenuScreen(),
+        routes: [ // 👇 Aggiungiamo le sotto-route
+         GoRoute(
+            path: 'quick-size',
+            pageBuilder: (context, state) => _animatedPage(
+              state: state,
+              child: const MenuScreen(selectedTab: 0),
+            ),
+          ),
+          GoRoute(
+            path: 'history',
+            pageBuilder: (context, state) => _animatedPage(
+              state: state,
+              child: const MenuScreen(selectedTab: 1),
+            ),
+          ),
+          GoRoute(
+            path: 'groups',
+            pageBuilder: (context, state) => _animatedPage(
+              state: state,
+              child: const MenuScreen(selectedTab: 2),
+            ),
+          ),
+          GoRoute(
+            path: 'profile',
+            pageBuilder: (context, state) => _animatedPage(
+              state: state,
+              child: const MenuScreen(selectedTab: 3),
+            ),
+          ),
+          
+        ],
       ),
     ],
+  );
+
+  // Funzione per animare la transizione
+
+}
+
+Page<dynamic> _animatedPage({required GoRouterState state, required Widget child}) {
+  return CustomTransitionPage(
+    key: state.pageKey,
+    child: child,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      return FadeTransition(
+        opacity: animation,
+        child: child,
+      );
+    },
   );
 }
