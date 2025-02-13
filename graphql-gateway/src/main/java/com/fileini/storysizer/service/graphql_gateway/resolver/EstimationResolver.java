d682bf4792403d740be2cfacc864ae9ef2f18468
fileini <francescomaria.falini@google.com>
2025-02-13 00:28:08 +0000

    edit gateway graphql

--- graphql-gateway/src/main/java/com/fileini/storysizer/service/graphql_gateway/resolver/EstimationResolver.java
+++ graphql-gateway/src/main/java/com/fileini/storysizer/service/graphql_gateway/resolver/EstimationResolver.java
@@ -3,6 +3,8 @@ package com.fileini.storysizer.service.graphql_gateway.resolver;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+import java.util.Map;
 
 @Controller
 public class EstimationResolver {
@@ -10,10 +12,16 @@ public class EstimationResolver {
     private final RestTemplate restTemplate = new RestTemplate();
 
     @SchemaMapping(typeName = "Estimation", field = "reach")
-    public Story getStory(Estimation estimation) {
-        return restTemplate.getForObject(
-            "http://story-service:8080/stories/" + estimation.getStoryId(),
-            Story.class
-        );
+    public Map<String, Object> getStory(Map<String, Object> estimation) {
+        Long storyId = (Long) estimation.get("storyId");
+
+        if (storyId == null) {
+            return null;
+        }
+
+        String url = UriComponentsBuilder.fromHttpUrl("http://story-service.service-prod.svc.cluster.local:8080/stories/" + storyId)
+                .toUriString();
+
+        return restTemplate.getForObject(url, Map.class);
     }
 }
