f295af4d389e3ee9bb552260e0ef3e7516afef85
fileini <francescomaria.falini@google.com>
2025-02-14 14:11:04 +0000

    fix

--- graphql-gateway/src/main/java/com/fileini/storysizer/service/graphqlgateway/controller/GraphiQLController.java
+++ graphql-gateway/src/main/java/com/fileini/storysizer/service/graphqlgateway/controller/GraphiQLController.java
@@ -1,4 +1,4 @@
-package com.fileini.storysizer.graphqlgateway.controller;
+package com.fileini.storysizer.service.graphqlgateway.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
