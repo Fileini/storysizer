7502ded050b86fe193b03ee41611f03b2e29fa46
fileini <francescomaria.falini@google.com>
2025-02-14 11:31:10 +0000

    sec config

--- graphql-gateway/src/main/java/com/fileini/storysizer/service/graphqlgateway/security/SecurityConfig.java
+++ graphql-gateway/src/main/java/com/fileini/storysizer/service/graphqlgateway/security/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
