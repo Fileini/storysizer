97be9ce3c61a0dc863f391e77d8ecf150464038e
Fileini <francescomaria.falini@gmail.com>
2025-02-20 00:00:13 +0100

    fix

--- backend/story-service/src/main/java/com/fileini/storysizer/service/story/controller/StoryController.java
+++ backend/story-service/src/main/java/com/fileini/storysizer/service/story/controller/StoryController.java
@@ -26,8 +26,8 @@ public class StoryController {
         return repository.findById(id).orElseThrow();
     }
 
-    @GetMapping("/owner/{owner}")
-    public List<Story> getStoryByOwner(@PathVariable String owner) {
+    @GetMapping
+    public List<Story> getStoryByOwner(@RequestParam(required = false) String owner) {
         return repository.findAll().parallelStream().filter(s -> s.getOwner().equals(owner)).toList();
     }
 
