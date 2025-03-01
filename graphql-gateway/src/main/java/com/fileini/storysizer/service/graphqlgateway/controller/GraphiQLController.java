92077e2959997d7e83ea5d856bd79514838567d6
Fileini <francescomaria.falini@gmail.com>
2025-03-01 15:54:14 +0100

    fix graphiql

--- graphql-gateway/src/main/java/com/fileini/storysizer/service/graphqlgateway/controller/GraphiQLController.java
+++ graphql-gateway/src/main/java/com/fileini/storysizer/service/graphqlgateway/controller/GraphiQLController.java
@@ -1,13 +1,21 @@
 package com.fileini.storysizer.service.graphqlgateway.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GraphiQLController {
 
     @GetMapping("/graphiql")
-    public String graphiql() {
+    public String graphiql(Model model, Authentication authentication) {
+        String token = "";
+        if (authentication != null && authentication.getPrincipal() instanceof Jwt) {
+            token = ((Jwt) authentication.getPrincipal()).getTokenValue();
+        }
+        model.addAttribute("authToken", token);
         return "graphiql";
     }
 }
