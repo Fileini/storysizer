74d3b98c3cd6b424658186fb26e8d552baf9c4f8
Fileini <francescomaria.falini@gmail.com>
2025-09-04 13:00:15 +0200

    debug

--- graphql-gateway/src/main/java/com/fileini/storysizer/service/graphqlgateway/resolver/EstimationFieldResolver.java
+++ graphql-gateway/src/main/java/com/fileini/storysizer/service/graphqlgateway/resolver/EstimationFieldResolver.java
@@ -1,12 +1,11 @@
 package com.fileini.storysizer.service.graphqlgateway.resolver;
 
+import com.fileini.storysizer.service.graphqlgateway.model.Estimation;
+import com.fileini.storysizer.service.graphqlgateway.model.Story;
 import graphql.kickstart.tools.GraphQLResolver;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
-
-import com.fileini.storysizer.service.graphqlgateway.model.Estimation;
-import com.fileini.storysizer.service.graphqlgateway.model.Story;
 
 @Component
 public class EstimationFieldResolver implements GraphQLResolver<Estimation> {
@@ -14,7 +13,7 @@ public class EstimationFieldResolver implements GraphQLResolver<Estimation> {
     private final RestTemplate restTemplate = new RestTemplate();
     private final String baseUrlStory = "http://story-service.service-prod.svc.cluster.local:8080/stories";
 
-    // Risolve: Estimation.story : Story
+    // matcha il campo GraphQL: Estimation.story : Story!
     public Story story(Estimation estimation) {
         Object storyId = estimation.get("storyId");
         if (storyId == null) return null;
